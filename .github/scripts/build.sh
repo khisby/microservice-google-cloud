113fadf0b6daae0ec61254c3e44507f4cdf32023
Khisby Al Ghofari <khisby@gmail.com>
2022-10-02 01:31:49 +0700

    [dev] try to build multi service 14

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -10,31 +10,32 @@ do
 
           cd "$path"
           
-          echo "Building and Deploying $app_name"
-          echo "GITHUB SHA $GITHUB_SHA"
-          echo "GITHUB REF $GITHUB_REF"
+          echo "================= Building and Deploying $app_name ================="
+          echo "================= GITHUB SHA $GITHUB_SHA ================="
+          echo "================= GITHUB REF $GITHUB_REF ================="
 
           # copy asset to directory
           cp -r ../../../.github/assets/* .
 
           # create deployment.yaml
-          sed -e "s/{APP_NAME}/$app_name/g" -e "s/{TAG}/$TAG/g" deployment.yaml.template > deployment.yaml
+          sed -e "s/{APP_NAME}/$app_name/g" -e "s/{TAG}/$tag/g" deployment.yaml.template > deployment.yaml
 
           # build the app
           docker build --tag "$tag" --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg GITHUB_REF="$GITHUB_REF" .
-          echo "Build Done"
+          echo "================= Build Done ================="
 
           # push the app
           docker push "$tag"
-          echo "Push Done"
+          echo "================= Push Done ================="
           
-          # deployment
-          ls
+          # remove later, just for debug
           cat deployment.yaml
+
+          # deployment
           kubectl apply -f deployment.yaml
-          kubectl rollout status deployment/staging
+          kubectl rollout status deployment/"$app_name"
           kubectl get services -o wide 
-          echo "Deploy Done"
+          echo "================= Deploy Done ================= "
 
           cd ../../../
 done
