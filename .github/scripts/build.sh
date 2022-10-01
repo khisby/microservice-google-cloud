d0df3b2d74a7a992e9c23ef0198ad7220010e2db
Khisby Al Ghofari <khisby@gmail.com>
2022-10-02 00:52:07 +0700

    [dev] try to build multi service 4

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,31 +1,35 @@
 #!/usr/bin/env bash
 
 search_dir=source/backend
+
 for entry in "$search_dir"/*
 do
           path="./$entry"
           app_name=$(basename $path)
+          tag="gcr.io/$PROJECT_ID/$app_name:$GITHUB_SHA"
+
           cd "$path"
           
           echo "Building and Deploying $app_name"
           echo "GITHUB SHA $GITHUB_SHA"
           echo "GITHUB REF $GITHUB_REF"
 
+          # copy asset to directory
+          cp -r ../../../.github/scripts/assets/* .
+
+          # create deployment.yaml
+          sed -e "s/\${APP_NAME}/$app_name/g" -e "s/\${TAG}/$TAG/g" deployment.yaml.template > deployment.yaml
+
           # build the app
-          docker build --tag "gcr.io/khisoft/$app_name:$GITHUB_SHA" --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg GITHUB_REF="$GITHUB_REF" .
+          docker build --tag "$tag" --build-arg GITHUB_SHA="$GITHUB_SHA" --build-arg GITHUB_REF="$GITHUB_REF" .
           echo "Build Done"
 
           # push the app
-          docker push "gcr.io/khisoft/$app_name:$GITHUB_SHA"
+          docker push "$tag"
           echo "Push Done"
           
           # set up kustomize 
-          curl -sfLo kustomize https://github.com/kubernetes-sigs/kustomize/releases/download/v3.1.0/kustomize_3.1.0_linux_amd64
-          chmod u+x ./kustomize
-          echo "Setup Kustomize Done"
-
-          ./kustomize edit set image gcr.io/PROJECT_ID/IMAGE:TAG=gcr.io/khisoft/$app_name:$GITHUB_SHA
-          ./kustomize build . | kubectl apply -f -
+          kubectl apply -f deployment.yml
           kubectl rollout status deployment/staging
           kubectl get services -o wide 
           echo "Deploy Done"
